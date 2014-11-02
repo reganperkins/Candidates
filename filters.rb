ab1ca1aa86a2a47747c2e5ed34fe5d1aabd30a28
Regan Perkins <reganlperkins@gmail.com>
2014-11-02 03:44:38 +0000

    fisrt commit

--- filters.rb
+++ filters.rb
@@ -1,11 +1,51 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
+require './candidates'
 
+def find(id)
+  @candidates.detect { |candidate| candidate[:id] == id.to_i }
+end
+
+def language_contains?(candidate, *req_languages)
+  languages = get_languages(candidate)
+  req_languages.each { |lang| return languages.include?(lang) }
+end
+
+def get_languages(candidate)
+  candidate[:languages]
 end
 
 def experienced?(candidate)
+  candidate[:years_of_experience] >= 2 
 end
 
-# More methods will go below
+def github_points(candidate)
+  candidate[:github_points] >= 100
+end
+
+def submit_date(candidate)
+  candidate[:date_applied] > 15.day.ago.to_date
+end
+
+def min_age(candidate)
+  candidate[:age] > 17
+end
+
+
+def qualified_candidates
+  @candidates.select do |candidate|
+    experienced?(candidate) && github_points(candidate) && min_age(candidate) && language_contains?(candidate, "Ruby", "Python") && submit_date(candidate)
+  end
+end
+
+def ordered_by_qualifications(candidates)
+  ordered_candidates = candidates.sort do |first_candidate, second_candidate|
+     if first_candidate[:years_of_experience] == second_candidate[:years_of_experience] 
+       second_candidate[:github_points] <=> first_candidate[:github_points]
+    else
+       second_candidate[:years_of_experience] <=> first_candidate[:years_of_experience]
+    end
+  end
+end
+
--- main.rb
+++ main.rb
@@ -7,6 +7,41 @@ require './filters'
 
 ## Your test code can go here
 
-# binding.pry
 
-pp qualified_candidates
+#binding.pry
+
+# pp qualified_candidates
+
+
+#call def 
+
+# puts experienced?(@candidates[1])
+# puts find(15)
+
+# puts qualified_candidates
+
+ordered_by_qualifications
+
+def repl
+  in_play = true
+  while in_play
+    puts "Wanna Play?"
+    user_input = gets.chomp
+
+    #puts user_input
+
+    case user_input
+    when /find (\d+)/
+      puts find($1)
+    when 'all' 
+      @candidates.each { |candidate| puts candidate }
+    when 'qualified' 
+      puts qualified_candidates(ordered_by_qualifications(@candidates))
+    when 'quit' 
+      in_play = false
+    end
+  end
+
+end
+
+repl
